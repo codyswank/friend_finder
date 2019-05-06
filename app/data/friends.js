c23c1f1817a5805900ddb2c4cbca748635de19be
Cody Swank <cody.swank1@gmail.com>
2019-05-05 22:15:44 -0400

    created Program

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+	
+	{
+		"name": "James Harden",
+		"photo": "https://theundefeated.com/wp-content/uploads/2019/04/APTOPIX-Jazz-Rockets-Basketball_35032829-e1556576053531.jpg?w=700",
+		"scores": [
+			"5",
+			"1",
+            "4",
+            "5",
+			"1",
+			"4",
+			"3",
+			"3",
+			"1",
+			"1"
+		]
+	},
+	{
+		"name": "Tom Brady",
+		"photo": "https://peopledotcom.files.wordpress.com/2019/04/tom-brady.jpg?w=2000&h=1591",
+		"scores": [
+			"3",
+			"1",
+			"5",
+			"1",
+            "5",
+            "4",
+			"1",
+			"5",
+			"1",
+			"3"
+		]
+	},
+	{
+		"name": "Mike Trout",
+		"photo": "https://a57.foxnews.com/media2.foxnews.com/BrightCove/694940094001/2019/03/19/931/524/694940094001_6015676392001_6015678286001-vs.jpg?ve=1&tl=1",
+		"scores": [
+			"3",
+			"5",
+			"1",
+            "5",
+            "3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"4"		
+		]
+	},
+	{
+		"name": "Cam Newton",
+		"photo": "https://s.yimg.com/ny/api/res/1.2/5b5zpbKu_EoRkQ8JNY7_Rg--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://img.huffingtonpost.com/asset/5ccc6030240000a000270aa3.jpeg",
+		"scores": [
+			"3",
+			"1",
+            "5",
+            "4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"4"		
+		]
+	},
+	{
+		"name": "Kobe Bryant",
+		"photo": "https://image.cnbcfm.com/api/v1/image/101524695-457220551.jpg?v=1395781183&w=1400&h=950",
+		"scores": [
+			"3",
+            "5",
+            "3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"1",
+			"5",
+			"4"		
+		]
+	},
+	{
+		"name": "Bryce Harper",
+		"photo": "https://www.philly.com/resizer/U3wy6AoC-epYUJ6lSNirVgdFCw4=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/TIY4TUKWDNELVGWLNWU7VLVKPQ.jpg",
+		"scores": [
+			"4",
+			"3",
+			"5",
+            "1",
+            "3",
+			"1",
+			"4",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
